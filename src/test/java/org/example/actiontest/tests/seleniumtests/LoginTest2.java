4709e71cb78eb7a5fb1c6b6a5601c3990e4b01d7
GA84019 <gguidice@ddic.delta.org>
2023-11-01 09:08:58 -0400

    my new caption

---
+++ src/test/java/org/example/actiontest/tests/seleniumtests/LoginTest2.java
@@ -0,0 +1,37 @@
+package org.example.actiontest.tests.seleniumtests;
+
+//import org.delta.automation.automationcoreframework.util.ApplicationProperties;
+//import org.delta.automation.selenium.automationbaseclasses.BaseSeleniumTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Test class for seleium.
+ */
+//public class LoginTest2 extends BaseSeleniumTest {
+public class LoginTest2{
+    /**
+     * The site url.
+     */
+    private static String siteAutomationUrl = "https://www.google.com/";
+
+    /**
+     * Test case for verifying the url matches our expectations.
+     */
+    @Test
+    public void getBrowserTest() {
+        WebDriver driver;
+
+        //Setting system properties of ChromeDriver
+        System.setProperty("webdriver.chrome.driver", "webdriver\\chromedriver.exe");
+
+        //Creating an object of ChromeDriver
+        driver = new ChromeDriver();
+
+        //launching the specified URL
+        driver.get(siteAutomationUrl);
+        driver.close();
+    }
+}
